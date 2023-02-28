c75bcfc1da8bccca3943d920b030766a9cc1206d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-28 12:07:33 +0300

    configure index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
+    <Provider store={store}>
       <App />
-    </>
+    </Provider>
   </React.StrictMode>,
 );
