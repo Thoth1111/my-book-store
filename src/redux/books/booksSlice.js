587bf698f42e2aa4145e53af6ff56dee24c8315b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-01 16:36:52 +0300

    Add AsyncThunk fetch method

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,11 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+const BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/:R3IvNOxbnH2rWy9AVC3x/books';
 
 const initialState = {
   books: [],
   status: 'idle',
   error: null,
 };
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    return [...response.data];
+  } catch (error) {
+    return error.message;
+  }
+});
 
 export const booksSlice = createSlice({
   name: 'books',
@@ -15,12 +27,12 @@ export const booksSlice = createSlice({
       const { title, author } = action.payload;
       if (title && author) {
         const newBook = { id: uuidv4(), title, author };
-        state.push(newBook);
+        state.books.push(newBook);
       }
     },
     removeBook: (state, action) => {
       const index = action.payload;
-      state.splice(index, 1);
+      state.books.splice(index, 1);
     },
   },
 });
