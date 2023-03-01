c8248adff6e757c22dbaec590c7bcd7a2b4d65f8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-01 15:59:11 +0300

    Refactor bookSlice initial state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
-const initialState = [
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
-];
+const initialState = {
+  books: [],
+  status: 'idle',
+  error: null,
+};
 
 export const booksSlice = createSlice({
   name: 'books',
