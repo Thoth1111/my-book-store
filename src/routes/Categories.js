8528ecca0740425a6ff4d93deeb4057caa4dd779
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-04 14:44:34 +0300

    Style category component

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categoriesSlice';
+import '../styles/books.css';
 
 function Categories() {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className="categ-cont">
       <button
         type="button"
+        className="btn-type2"
         onClick={() => dispatch(checkStatus('Under construction'))}
       >
         Check status
